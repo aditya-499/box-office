72200a22ea6da096bd0f0c3480787b100d9cd5cd
Aditya <itsmeaditya004@gmail.com>
2022-05-27 08:56:15 +0530

    added pages with react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is Home
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>Error 404, Page not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
